2efe822fe1f7f3837b2505cdf6a7ede77f7ceafc
João Vitor Melo Kremer <joaokremer.dev@gmail.com>
2023-03-31 19:21:19 -0300

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,5 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [ svgr(), react()],
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
-      output: {
-        globals: {
-          'react-router-dom': 'ReactRouterDOM'
-        }
-      }
-    }
-  }
+  plugins: [ svgr(), react()]
 })
